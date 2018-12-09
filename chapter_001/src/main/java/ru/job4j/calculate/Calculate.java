dca5c28348f89e1cc7f68f1cfa8fc2adc9ce62be
VyacheslavBulatov <Bul290896@yandex.ru>
2018-12-09 21:12:41 +0300

    1.3. JavaDoc. Code convention[#88980]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ *Calculate.
+ *
+ *@author Viacheslav Bulatov (bul290896@yandex.ru)
+ */
+
 public class Calculate{
-	
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
