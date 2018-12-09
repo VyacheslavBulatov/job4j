cd4ffee5e1bf615a22d5caf4a062e719d3f13dc6
VyacheslavBulatov <Bul290896@yandex.ru>
2018-12-09 22:05:01 +0300

    1.5. Checkstyle[#88982]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,12 +6,12 @@ package ru.job4j.calculate;
  *@author Viacheslav Bulatov (bul290896@yandex.ru)
  */
 
-public class Calculate{
+public class Calculate {
 	/**
 	 * Main.
 	 * @param args - args.
 	 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 	/**
