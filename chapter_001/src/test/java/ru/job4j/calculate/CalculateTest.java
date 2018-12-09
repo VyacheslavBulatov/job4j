55280c2d45e38cfe459c18ecbd44bac518f3edc2
VyacheslavBulatov <Bul290896@yandex.ru>
2018-12-09 21:55:19 +0300

    1.4. JUnit[#88981]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Viacheslav Bulatov (bul290896@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/
+@Test
+public void whenTakeNameThenThreeEchoPlusName() {
+    String input = "Viacheslav Bulatov";
+    String expect = "Echo, echo, echo : Viacheslav Bulatov"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
+ 
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate test.
+ *
+ * @author Viacheslav Bulatov
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
