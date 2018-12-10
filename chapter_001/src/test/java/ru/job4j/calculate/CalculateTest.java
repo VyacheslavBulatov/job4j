b1fad6aece689090a049f357a0ea250c229bcc4a
VyacheslavBulatov <Bul290896@yandex.ru>
2018-12-11 00:13:57 +0300

    2.3. Запуск программы, запуск тестов, дебаг.[#88937]

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,24 +5,24 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Viacheslav Bulatov (bul290896@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Viacheslav Bulatov (bul290896@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenThreeEchoPlusName() {
-    String input = "Viacheslav Bulatov";
-    String expect = "Echo, echo, echo : Viacheslav Bulatov"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "Viacheslav Bulatov";
+        String expect = "Echo, echo, echo : Viacheslav Bulatov";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
  
