ca5429e0bb2760706e33f8b5e701a56ce367b6a3
VyacheslavBulatov <Bul290896@yandex.ru>
2018-12-12 21:47:29 +0300

    JavaDoc для калькулятора

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,24 +1,51 @@
 package ru.job4j.calculator;
 
+/**
+ * Элементарный калькулятор
+ */
 public class Calculator {
     private double result;
 
+    /**
+     * Вычисление суммы двух чисел
+     * @param first первое число
+     * @param second второе число
+     */
     public void add(double first, double second) {
         this.result = first + second;
     }
 
+    /**
+     * Вычисление разности двух чисел
+     * @param first первое число
+     * @param second второе число
+     */
     public void subtract(double first, double second) {
         this.result = first - second;
     }
 
+    /**
+     * Вычисление частного двух чисел
+     * @param first первое число
+     * @param second второе число
+     */
     public void div(double first, double second) {
         this.result = first / second;
     }
 
+    /**
+     * Вычисление произведения двух чисел
+     * @param first первое число
+     * @param second второе число
+     */
     public void multiple(double first, double second) {
         this.result = first * second;
     }
 
+    /**
+     * Возвращает результат вычисления
+     * @return результат операции
+     */
     public double getResult() {
         return this.result;
     }
