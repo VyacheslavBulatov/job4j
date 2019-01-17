de3fdd9de96c1d27916a2ce0b98bcda58e83044a
VyacheslavBulatov <Bul290896@yandex.ru>
2019-01-17 15:58:01 +0300

    Объединение двух массивов. (Доработано)

--- chapter_001/src/main/java/ru/job4j/array/Union.java
+++ chapter_001/src/main/java/ru/job4j/array/Union.java
@@ -17,29 +17,13 @@ public class Union {
         int[] result = new int[first.length + second.length];
         int a = 0;
         int b = 0;
-        int c = 0;
-        for (int i = b; i < second.length; i++) {
-                for (int j = a; j < first.length; j++) {
-                    if (first[j] < second[i]) {
-                        result[c] = first[j];
-                        c++;
-                        a++;
-                    } else {
-                        result[c] = second[i];
-                        c++;
-                        b++;
-                        break;
-                    }
-                }
+        for (int i = 0; i < result.length; i++) {
+            if (a != first.length && b != second.length) {
+               result[i] = first[a] < second[b] ? first[a++] : second[b++];
+                } else {
+                result[i] = a != first.length ? first[a++] : second[b++];
             }
-        for (int i = a; i < first.length; i++) {
-                result[c] = first[i];
-                c++;
-        }
-        for (int i = b; i < second.length; i++) {
-            result[c] = second[i];
-            c++;
-        }
+            }
         return result;
     }
 }
