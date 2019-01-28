026d84b14659f542c90e83f7fbf9cbfbb1af7b61
VyacheslavBulatov <Bul290896@yandex.ru>
2019-01-29 00:14:06 +0300

    5. Рефакторинг теста @Before @After[#89015]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -8,11 +10,28 @@ import static org.junit.Assert.assertThat;
 
 
 public class PaintTest {
+    /**
+     * поле содержит дефолтный вывод в консоль.
+     */
+    private final PrintStream stdout = System.out;
+    /**
+     * буфер для результата.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
     @Test
     public void whenDrawSquareThenGetSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         String expected = new StringBuilder()
                 .append("+++++\n")
@@ -22,13 +41,9 @@ public class PaintTest {
                 .append(System.lineSeparator())
                 .toString();
         assertThat(new String(out.toByteArray()), is(expected));
-        System.setOut(stdout);
     }
     @Test
     public void whenDrawTriangleThenGetTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         String expected = new StringBuilder()
                 .append("   ^ \n ")
@@ -37,6 +52,5 @@ public class PaintTest {
                 .append(System.lineSeparator())
                 .toString();
         assertThat(new String(out.toByteArray()), is(expected));
-        System.setOut(stdout);
     }
 }
