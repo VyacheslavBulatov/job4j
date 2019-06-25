98a043d93dcde79c6aead7f708811171a64c9439
VyacheslavBulatov <Bul290896@yandex.ru>
2019-06-25 15:56:07 +0300

    2. Сортировка User с использованием Comparator[#88996]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,5 +1,6 @@
 package ru.job4j.sort;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -17,5 +18,43 @@ public class SortUser {
      */
     public Set<User> sort(List<User> users) {
         return new TreeSet<>(users);
+    }
+
+    /**
+     * Сортирует список пользователей по длине имени
+     * @param users исходный список
+     * @return отсортированный список
+     */
+    public List<User> sortNameLength(List<User> users) {
+        users.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return Integer.compare(o1.getName().length(), o2.getName().length());
+            }
+        });
+        return users;
+    }
+
+    /**
+     * Сортирует список пользователей сначала по имени в лексикографическом порядке, потом по возрасту
+     * @param users исходный список
+     * @return отсортированный список
+     */
+    public List<User> sortByAllFields(List<User> users) {
+        users.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int result;
+                String n1 = o1.getName();
+                String n2 = o2.getName();
+                if (!n1.equals(n2)) {
+                    result = o1.getName().compareTo(o2.getName());
+                } else {
+                    result = Integer.compare(o1.getAge(), o2.getAge());
+                }
+                return result;
+            }
+        });
+        return users;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -22,5 +22,32 @@ public class SortUserTest {
         assertEquals(it.next().getName(), "Vadyan");
         assertEquals(it.next().getName(), "Slava");
     }
-
+    @Test
+    public void whenAddFourUsersThanFindOne() {
+        SortUser sortUser = new SortUser();
+        List<User> users = new ArrayList<>();
+        users.add(new User("Petr", 40));
+        users.add(new User("Slava", 23));
+        users.add(new User("Vadyan", 18));
+        users.add(new User("Petr", 50));
+        List<User> result = sortUser.sortByAllFields(users);
+        assertEquals(result.get(3).getName(), "Vadyan");
+        assertEquals(result.get(1).getName(), "Petr");
+        assertEquals(result.get(2).getName(), "Slava");
+        assertEquals(result.get(1).getAge(), 50);
+    }
+    @Test
+    public void whenAddFiveUsersThanFindOne() {
+        SortUser sortUser = new SortUser();
+        List<User> users = new ArrayList<>();
+        users.add(new User("Petr", 40));
+        users.add(new User("Slava", 23));
+        users.add(new User("Vadyan", 18));
+        users.add(new User("Viacheslav", 50));
+        users.add(new User("Alexandr", 50));
+        List<User> result = sortUser.sortNameLength(users);
+        assertEquals(result.get(2).getName(), "Vadyan");
+        assertEquals(result.get(0).getName(), "Petr");
+        assertEquals(result.get(4).getName(), "Viacheslav");
+    }
 }
